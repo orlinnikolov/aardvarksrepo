9cc71492f9dfd71d9cca5ef5f8f4a201cf64ee25
Orlin Nikolov <Orlin@forsightprudence.com>
2012-12-14 16:54:59 +0200

    init commit

---
+++ GitProject/src/git/Main.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gogo");
+	}
+
+}
